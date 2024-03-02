5aeb0dc6f63f9050aefbddbddee1168ee0d869bf
nebstarmalala <nebstartrizzle@gmail.com>
2024-03-02 12:42:12 +0300

    add product prefix for categories

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -5,22 +5,22 @@ function Categories() {
   return (
     <section className="categories">
       <div className="category-item">
-        <Link to="/women" className="category-btn">
+        <Link to="/products/women" className="category-btn">
           Women
         </Link>
       </div>
       <div className="category-item">
-        <Link to="/men" className="category-btn">
+        <Link to="/products/men" className="category-btn">
           Men
         </Link>
       </div>
       <div className="category-item">
-        <Link to="/accessories" className="category-btn">
+        <Link to="/products/accessories" className="category-btn">
           Accessories
         </Link>
       </div>
       <div className="category-item">
-        <Link to="/footwear" className="category-btn">
+        <Link to="/products/footwear" className="category-btn">
           Footwear
         </Link>
       </div>
