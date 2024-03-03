16b3fca3b332b473be0e46166d59935bfd71b3f0
nebstarmalala <nebstartrizzle@gmail.com>
2024-03-03 10:05:36 +0300

    add use authe check hook

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,8 +2,10 @@ import Banner from "../components/Banner";
 import Navbar from "../components/Navbar";
 import Categories from "../components/categories";
 import Advert from "../features/advert";
+import useAuthCheck from '../features/useAuthCheck';
 
 function Homepage() {
+  useAuthCheck();
   return (
     <>
       <Navbar />
